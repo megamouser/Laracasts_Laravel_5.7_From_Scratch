543e3326e43852b062ea2d1292ca6d2a892851b5
Artem Yurev <artemon-yrev@yandex.ru>
2019-05-04 02:04:03 +0300

    17

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,12 +2,23 @@
 @section('content')
 <div class="container">
     <h1 class="title">{{ $project->title }}</h1>
+
     <hr>
-    <div class="content">
+
+    <div>
         {{ $project->description }}
     </div>
+    
+    @if ($project->tasks->count())
+        <div>
+            @foreach($project->tasks as $task)
+                <li>{{ $task->description }}</li>
+            @endforeach
+        </div>
+    @endif
+    
     <div>
-        <a href="/projects/{{ $project->id }}/edit">edit</a>
+        <a href="/projects/{{ $project->id }}/edit">Edit</a>
     </div>
 </div>
 @endsection
